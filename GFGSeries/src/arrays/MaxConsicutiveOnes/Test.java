4260c76f6a0beeddcdb2dff7498f24b6ddaf64f2
Lenovo <Lenovo@DESKTOP-6VF9S7M>
2025-01-14 21:41:40 +0530

    max consicutive 1s move all 1s to end move all negative to starting

---
+++ GFGSeries/src/arrays/MaxConsicutiveOnes/Test.java
@@ -0,0 +1,26 @@
+package arrays.MaxConsicutiveOnes;
+
+public class Test {
+
+	public static void maxConsicutiveOnes(int arr[]) {
+		int count = 0;
+		int maxCount = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 1) {
+				count++;
+				if (count > maxCount) {
+					maxCount = count;
+				}
+			} else {
+				count = 0;
+			}
+		}
+		System.out.println(maxCount);
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1 };
+		maxConsicutiveOnes(arr);
+	}
+
+}
