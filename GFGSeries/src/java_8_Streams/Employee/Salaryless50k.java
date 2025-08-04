2fed846d51ccbb67ffdac9a2192bef4ed70ebd35
Puru <Puru@host.docker.internal>
2025-08-04 14:27:08 +0530

    removes

--- GFGSeries/src/java_8_Streams/Employee/Salaryless50k.java
+++ GFGSeries/src/java_8_Streams/Employee/Salaryless50k.java
@@ -1,8 +1,12 @@
 package java_8_Streams.Employee;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Salaryless50k {
 
@@ -10,8 +14,26 @@ public class Salaryless50k {
 		emp.stream().filter(e->e.getSalary()<50000L).map(e->e.getEmployeeName()).forEach(e->System.out.println(e + ","));
 	}
 
-	public static void main(String[] args) {
+	public static void highestSalary(List<Employee> emp) {
+		System.out.println(emp.stream().sorted(Comparator.comparing(Employee::getSalary).reversed()).map(e->e.getEmployeeName()).findFirst().get());
 
+		
+		//emp.stream().sorted(Comparator.comparing(Employee::getSalary).reversed()).findFirst();
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	//	List<String> lst = List.of("Ansh,");
+		
+		Map<String,Integer> mp= new HashMap<>();
+		mp.put("Ansh", 21);
+		mp.put("Ansh1", 29);
+		System.out.println(	mp.entrySet().stream().sorted(Comparator.comparing(Map.Entry<String,Integer>::getValue).reversed()).map(e->e.getKey()).findFirst().get());
+	
+		
 		List<Employee> listOfEmp = new ArrayList<>();
 		listOfEmp.add(new Employee(1, "Sahil", 50000L, new Date(2010, 01, 12), "Operations"));
 		listOfEmp.add(new Employee(2, "Puru", 100000L, new Date(2012, 01, 12), "Development"));
@@ -21,7 +43,8 @@ public class Salaryless50k {
 		listOfEmp.add(new Employee(6, "Ganesh", 30000L, new Date(2010, 01, 12), "Operations"));
 		listOfEmp.add(new Employee(7, "Rahul", 90000L, new Date(2020, 01, 12), "HR"));
 
-		findemployee(listOfEmp);
+		//findemployee(listOfEmp);
+		//highestSalary(listOfEmp);
 
 	}
 
